af905140e6dc6600a3b3b18ddfe77ae2848d414a
Heytor Cavalcanti <heytor.c.moura@accenture.com>
2021-11-12 20:13:05 -0300

    fix: steam return url

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,8 @@ const SteamAuth = require("node-steam-openid");
 
 const steam = new SteamAuth({
   realm: process.env.HOST, // Site name displayed to users on logon
-  returnUrl: `${process.env.HOST}:${process.env.PORT}/auth/steam/authenticate`, // Your return route
+  //returnUrl: `${process.env.HOST}:${process.env.PORT}/auth/steam/authenticate`, // Your return route
+  returnUrl: `${process.env.HOST}/auth/steam/authenticate`, // Your return route
   apiKey: process.env.STEAM_API_KEY // Steam API key
 });
 
